178a651bbd6a20137f8a0aedaa7ec94f6ed52878
Max Andreev <maksandre@yandex.ru>
2021-03-26 14:06:55 +0300

    remove WithRateList
    because it works tricky

--- src/screens/InflationScreen.jsx
+++ src/screens/InflationScreen.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Alert, LogBox, StyleSheet, View } from 'react-native';
 import { connect } from 'react-redux';
+import AppView from '../components/AppView';
+import AppCreateButton from '../components/buttons/AppCreateButton';
+import EconomyList from '../components/lists/economy/EconomyList';
+import RateModal from '../components/modal/RateModal';
+import { AppSubtitle } from '../components/text';
 import {
   createInflationRate,
   deleteInflationRate,
   getInflationRates,
   updateInflationRate,
 } from '../store/actions/inflation';
-import WithRateList from './WithRateList';
+
+LogBox.ignoreLogs(['VirtualizedLists should never be nested']); // TODO wait for fix on github
+
+const itemInitialState = { name: '', rate: '' };
 
 const InflationScreen = ({
   inflationRates,
@@ -15,19 +24,96 @@ const InflationScreen = ({
   updateInflationRate,
   deleteInflationRate,
 }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(itemInitialState);
+
+  useEffect(() => {
+    getInflationRates();
+  }, []);
+
+  const handleClose = () => {
+    setModalVisible(false);
+    setSelectedItem(itemInitialState);
+  };
+
+  const handleSubmit = () => {
+    if (selectedItem.id) {
+      updateInflationRate(selectedItem);
+    } else {
+      createInflationRate({
+        ...selectedItem,
+        position:
+          inflationRates.length > 0
+            ? inflationRates.sort((a, b) => b.position - a.position)[0]
+                .position + 1
+            : 0,
+      });
+    }
+    handleClose();
+  };
+
+  const handleDelete = () => {
+    Alert.alert('Attention!', `You want to delete «${selectedItem.name}»?`, [
+      { text: 'Cancel', style: 'cancel' },
+      {
+        text: 'Delete',
+        style: 'destructive',
+        onPress: () => {
+          deleteInflationRate(selectedItem.id);
+          handleClose();
+        },
+      },
+    ]);
+  };
+
+  const handleItemPress = (item) => {
+    setSelectedItem(item);
+    setModalVisible(true);
+  };
+
+  const handleDragEnd = (data) => {
+    updateInflationRate(data);
+  };
+
   return (
-    <WithRateList
-      data={inflationRates}
-      onLoad={getInflationRates}
-      createFunction={createInflationRate}
-      updateFunction={updateInflationRate}
-      deleteFunction={deleteInflationRate}
-      title="Inflation rates"
-      modalTitlePlaceholder="Inflation rate name"
-      modalSubtitle="Inflation rate"
-    />
+    <AppView>
+      <RateModal
+        item={selectedItem}
+        titlePlaceholder={'Inflation rate name'}
+        subtitle={'Inflation rate'}
+        onTitleChange={(value) =>
+          setSelectedItem({ ...selectedItem, name: value })
+        }
+        onRateChange={(value) =>
+          setSelectedItem({ ...selectedItem, rate: value.trim() })
+        }
+        onDelete={handleDelete}
+        isVisible={modalVisible}
+        onClose={handleClose}
+        onSubmit={handleSubmit}
+      />
+      <View style={styles.titleContainer}>
+        <AppSubtitle>Inflation rates</AppSubtitle>
+        <AppCreateButton onPress={() => handleItemPress(itemInitialState)} />
+      </View>
+      <EconomyList
+        data={inflationRates.sort((a, b) => b.position - a.position)}
+        onPress={handleItemPress}
+        onDragEnd={handleDragEnd}
+        onAddPress={() => handleItemPress(itemInitialState)}
+      />
+    </AppView>
   );
 };
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginVertical: 10,
+  },
+});
 
 function mapStateToProps(state) {
   return {
--- src/screens/YieldsScreen.jsx
+++ src/screens/YieldsScreen.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Alert, LogBox, StyleSheet, View } from 'react-native';
 import { connect } from 'react-redux';
+import AppView from '../components/AppView';
+import AppCreateButton from '../components/buttons/AppCreateButton';
+import EconomyList from '../components/lists/economy/EconomyList';
+import RateModal from '../components/modal/RateModal';
+import { AppSubtitle } from '../components/text';
 import {
   createYield,
   deleteYield,
   getYields,
   updateYield,
 } from '../store/actions/economy';
-import WithRateList from './WithRateList';
+
+LogBox.ignoreLogs(['VirtualizedLists should never be nested']); // TODO wait for fix on github
+
+const itemInitialState = { name: '', rate: '' };
 
 const YieldsScreen = ({
   yields,
@@ -15,19 +24,95 @@ const YieldsScreen = ({
   updateYield,
   deleteYield,
 }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(itemInitialState);
+
+  useEffect(() => {
+    getYields();
+  }, []);
+
+  const handleClose = () => {
+    setModalVisible(false);
+    setSelectedItem(itemInitialState);
+  };
+
+  const handleSubmit = () => {
+    if (selectedItem.id) {
+      updateYield(selectedItem);
+    } else {
+      createYield({
+        ...selectedItem,
+        position:
+          yields.length > 0
+            ? yields.sort((a, b) => b.position - a.position)[0].position + 1
+            : 0,
+      });
+    }
+    handleClose();
+  };
+
+  const handleDelete = () => {
+    Alert.alert('Attention!', `You want to delete «${selectedItem.name}»?`, [
+      { text: 'Cancel', style: 'cancel' },
+      {
+        text: 'Delete',
+        style: 'destructive',
+        onPress: () => {
+          deleteYield(selectedItem.id);
+          handleClose();
+        },
+      },
+    ]);
+  };
+
+  const handleItemPress = (item) => {
+    setSelectedItem(item);
+    setModalVisible(true);
+  };
+
+  const handleDragEnd = (data) => {
+    updateYield(data);
+  };
+
   return (
-    <WithRateList
-      data={yields}
-      onLoad={getYields}
-      createFunction={createYield}
-      updateFunction={updateYield}
-      deleteFunction={deleteYield}
-      title="Yield rates"
-      modalTitlePlaceholder="Yield name"
-      modalSubtitle="Yield rate"
-    />
+    <AppView>
+      <RateModal
+        item={selectedItem}
+        titlePlaceholder={'Yield name'}
+        subtitle={'Yield rate'}
+        onTitleChange={(value) =>
+          setSelectedItem({ ...selectedItem, name: value })
+        }
+        onRateChange={(value) =>
+          setSelectedItem({ ...selectedItem, rate: value.trim() })
+        }
+        onDelete={handleDelete}
+        isVisible={modalVisible}
+        onClose={handleClose}
+        onSubmit={handleSubmit}
+      />
+      <View style={styles.titleContainer}>
+        <AppSubtitle>Yield rates</AppSubtitle>
+        <AppCreateButton onPress={() => handleItemPress(itemInitialState)} />
+      </View>
+      <EconomyList
+        data={yields.sort((a, b) => b.position - a.position)}
+        onPress={handleItemPress}
+        onDragEnd={handleDragEnd}
+        onAddPress={() => handleItemPress(itemInitialState)}
+      />
+    </AppView>
   );
 };
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginVertical: 10,
+  },
+});
 
 function mapStateToProps(state) {
   return {

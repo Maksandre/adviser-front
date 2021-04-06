b278f65edabf34ddfbff17231bdac9e00accef2b
Max Andreev <maksandre@yandex.ru>
2021-04-06 13:07:18 +0300

    some small fixes

--- src/screens/WealthSummaryScreen.jsx
+++ src/screens/WealthSummaryScreen.jsx
@@ -1,36 +1,11 @@
 import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
-import { useState } from 'react/cjs/react.development';
+import { Text, StyleSheet } from 'react-native';
 import AppView from '../components/AppView';
-import AppPicker from '../components/inputs/AppPicker';
-import { COLOR } from '../constants/colors';
-
-const months = [
-  '. . .',
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-];
 
 const WealthSummaryScreen = () => {
-  const [value, setValue] = useState('May');
-
   return (
     <AppView style={styles.container}>
-      <AppPicker
-        value={value}
-        elements={months}
-        onChange={(text) => setValue(text)}
-      />
+      <Text>Wealth screen</Text>
     </AppView>
   );
 };
